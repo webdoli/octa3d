7a6e191661ac5008a740c97148e1e1be8a7f1ac9
LEE JUHEON <topofweird@gmail.com>
2023-06-20 12:17:51 +0900

    ver0. deploy test01

--- packages/main/config/webpack.prod.js
+++ packages/main/config/webpack.prod.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const path = require('path');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
