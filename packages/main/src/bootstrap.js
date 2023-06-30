30388460bd293c4be0fc1fd067f1f145a779d4c8
LEE JUHEON <topofweird@gmail.com>
2023-06-30 13:24:58 +0900

    v1. navigo test

--- packages/main/src/bootstrap.js
+++ packages/main/src/bootstrap.js
@@ -7,7 +7,7 @@ import Navigo from 'navigo';
 import Login from './components/login';
 import Signup from './components/signup';
 
-const router = new Navigo('/');
+const router = new Navigo('https://octa3d-439a2.firebaseapp.com/');
 const headerEle = document.querySelector('.header-one');
 const mainEle = document.querySelector('#main');
 const footerEle = document.querySelector('.footer1');
